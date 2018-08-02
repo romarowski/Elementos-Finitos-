c3ed7430ca1a7f9d2b5abc55cecf197ae3912863
romarowski <36279027+romarowski@users.noreply.github.com>
2018-08-02 17:07:13 -0300

    solucion matriz por elemento

--- practica1_ej1.m
+++ practica1_ej1.m
@@ -6,3 +6,15 @@ E = 210e3;
 P = -1e3;
 f = @(x) P/E * L * (log(A_i*A_i)- 2*log((A_i*(L-x)+x*A_s)/L)) / 2* (A_i -A_s);
 fplot(f,[0,L]);
+N=1;
+nodes= 0:L/N:L;
+elem =zeros(N-1,2);
+for i=1:N
+    elem(i,:) = [i i+1];
+end
+areaele = @(x) A_i* (L-x) /L + A_s *x/L;
+k_g = zeros(N+1);
+for i=1:N
+    k_e = areaele(nodes(i)) * E / (L/N) * [1 -1 ; -1 1];
+    k_g =      
+end
