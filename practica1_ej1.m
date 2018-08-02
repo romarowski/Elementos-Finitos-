cd1fc604d5418dc61dde5e57f3c72c287cfa2170
romarowski <36279027+romarowski@users.noreply.github.com>
2018-08-02 14:40:02 -0300

    L faltantte

--- practica1_ej1.m
+++ practica1_ej1.m
@@ -4,5 +4,5 @@ A_s = 25;
 L = 4000;
 E = 210e3;
 P = -1e3;
-f = @(x) P/E * L * (log(A_i*A_i)- 2*log(A_i*(L-x)+x*A_s)) / 2* (A_i -A_s);
+f = @(x) P/E * L * (log(A_i*A_i)- 2*log((A_i*(L-x)+x*A_s)/L)) / 2* (A_i -A_s);
 fplot(f,[0,L]);
