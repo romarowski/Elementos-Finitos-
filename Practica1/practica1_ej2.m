21818e8a8cf77d4a68682c0981e1082b251a973b
romarowski <romarowski@wisc.edu>
2018-08-07 13:24:36 -0300

    ej2 ok

---
+++ Practica1/practica1_ej2.m
@@ -0,0 +1,33 @@
+syms A E L;
+hold on
+L = 60;
+A = 2;
+E = 30e6;
+t_x = @(x) -10 * x;
+maxelem = 50;
+for N=3:maxelem
+    nodes = 0:L/N:L;
+    elem = zeros(N,2);
+    for i=1:N
+        elem(i,:) = [i i+1];
+    end
+    k_g = zeros(N+1);
+    fzas = zeros(1,N);
+    for i=1:N
+        k_e = A * E / L * [1 -1 ; -1 1];
+        k_g(elem(i,:), elem(i,:)) = k_e + k_g(elem(i,:),elem(i,:));
+        fzapernode = integral(t_x,nodes(i), nodes(i+1));
+        fzas(i) = 1/3 * fzapernode;
+        if i ~= N
+        fzas(i+1) = 2/3 * fzapernode;
+        end
+    end
+    bc = zeros(1,N+1);
+    bc(end) = 1;
+    k_red = k_g(~bc,~bc);
+    despl = k_red ^ (-1) * fzas';
+    figure(1)
+    plot(nodes,[despl' , 0]);
+end
+ 
+    
