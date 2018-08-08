3d4e3c2983b593f3e0734516b6118c9d2ce89706
romarowski <romarowski@wisc.edu>
2018-08-08 18:50:26 -0300

    generalida

--- Practica1/practica1_ej2.m
+++ Practica1/practica1_ej2.m
@@ -12,20 +12,18 @@ for N=3:maxelem
         elem(i,:) = [i i+1];
     end
     k_g = zeros(N+1);
-    fzas = zeros(1,N);
+    fzas = zeros(1,N+1);
     for i=1:N
         k_e = A * E / L * [1 -1 ; -1 1];
         k_g(elem(i,:), elem(i,:)) = k_e + k_g(elem(i,:),elem(i,:));
         fzapernode = integral(t_x,nodes(i), nodes(i+1));
         fzas(i) = 1/3 * fzapernode;
-        if i ~= N
         fzas(i+1) = 2/3 * fzapernode;
-        end
     end
     bc = zeros(1,N+1);
     bc(end) = 1;
     k_red = k_g(~bc,~bc);
-    despl = k_red ^ (-1) * fzas';
+    despl = k_red ^ (-1) * fzas(1:end-1)';
     figure(1)
     plot(nodes,[despl' , 0]);
 end
